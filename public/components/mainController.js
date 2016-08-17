a76628e7f5e378a2a2d26e8212013675fcb0835b
William Elimbi <williamelimbi@gmail.com>
2016-08-17 13:04:40 +0200

    clean MainController.js files

--- public/components/mainController.js
+++ public/components/mainController.js
@@ -11,9 +11,8 @@
 
    $http.get('/api/stat').success(function (data) {
          vm.stats = data;
-         debugger;
+
          vm.teams = vm.stats.apiResults[0].league.season.eventType[0].conferences[0].divisions[0].teams;
-         //console.log(vm.stats.apiResults.[0].season.evenType[0].conferences.[]);
        });
 
   }
